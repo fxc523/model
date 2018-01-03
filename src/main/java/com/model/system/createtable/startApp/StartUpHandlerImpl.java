a21b1c81b6b656f63ee90e5bf793fad3e6ca9f06
fxc <785349014@qq.com>
2018-01-03 20:43:55 +0800

    警告消除

--- src/main/java/com/model/system/createtable/startApp/StartUpHandlerImpl.java
+++ src/main/java/com/model/system/createtable/startApp/StartUpHandlerImpl.java
@@ -14,8 +14,6 @@ package com.model.system.createtable.startApp;
 
 import javax.annotation.PostConstruct;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
